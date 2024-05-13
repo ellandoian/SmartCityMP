c5d77c6d4353da772a6a38ed5bb4648cff75fbc9
ellandoian <jjojjo3214@gmail.com>
2024-05-13 15:32:35 +0200

    added calibrate too "bomring"

--- bommring/bommring.ino
+++ bommring/bommring.ino
@@ -9,29 +9,37 @@ void setup() {
 }
 
 //kilde, ellandoian/filedump
-bool button(int trueTime)  //trueTime is how long you want the button to return TRUE
-//"pushButton" is the physical button, change name accordingly
-{
+bool button(int trueTime, bool pulldown) {
+  //trueTime is how long you want the button to return "true", input "true" if using a pulldown system or "false" if pullup
+  //"pushButton" is the physical button, change name accordingly
   static bool val, buttonVar, lastButtonState = false;
   static uint32_t timer;
-  if (digitalRead(pushButton) == HIGH) {
+  if (digitalRead(pushButton) == pulldown) {
     buttonVar = true;  // setter buttonVar til true mens knappen er klikket ned
     timer = millis();
   }
-  if ((digitalRead(pushButton) == LOW) && (buttonVar == true))  // vil kjøre når knappen slippes og endrer retur variablen
+  if ((digitalRead(pushButton) != pulldown) && (buttonVar == true))  // vil kjøre når knappen slippes og endrer retur variablen
   {
     val = true;
-    if (millis() - timer > trueTime) val, buttonVar = false;
+    if (millis() - timer > trueTime) {
+      val = false;
+      buttonVar = false;
+    }
   }
   return val;
 }
 
+short proxRead() {
+  static short proximity;
+  if (APDS.proximityAvailable()) proximity = APDS.readProximity();
+  return proximity;
+}
 
-int carCount(int proxy) {    //teller hvor mange biler som har kjørt forbi bommen, tar inn proximity dataen
-  static int cCounter = -1;  //vil autmatisk telle +1 når koden kjøres første gang, verdi på -1 gjør at den starter på 0
+short carCount(int proxy) {    //teller hvor mange biler som har kjørt forbi bommen, tar inn proximity dataen
+  static short cCounter = -1;  //vil autmatisk telle +1 når koden kjøres første gang, verdi på -1 gjør at den starter på 0
   static bool countState = false;
 
-  if (proxy < 235) countState = true;    //veien ligger på runt 242, redisterer når noe har kommet til bommen
+  if (proxy < 235) countState = true;    //veien ligger på runt 242, registerer når noe har kommet til bommen
   else if (proxy > 235 && countState) {  //når bilen har kjørt helt gjennom bommen, vil bilen bli telt
     cCounter++;
     countState = false;
@@ -48,35 +56,52 @@ int* colorRead() {
   return rgb;
 }
 
-int proxRead() {
-  static int proximity;
-  if (APDS.proximityAvailable()) proximity = APDS.readProximity();
-  return proximity;
-}
-/*int* calibrateCol() {
-  int* base;
-  if (button(1000)){
-//ta ett par målinger og gjennomsnitt eller no for å finne grei tilpassning av grunn fargen, siden den
-//pleier å endre litt på veridene de måler fra måling til måling tror jeg gjennomsnitt er smart
+int* calibrateCol() {  //tar 10 målinger over 1,2 sekunder og finner gjennomsnittet
+  static uint32_t colCalTime = millis();
+  static bool ccFlag = false;
+  static short count;
+  static int base[3], prevBase[3];
+  if (button(1200, true) && millis() - colCalTime >= 100) {  //hvert 100 millisekund tar den en måling,
+    int* read;
+    read = colorRead();
+    for (short i; i <= 2; i++) {
+      base[i] = base[i] + read[i];
+    }
+    count++;
+    colCalTime = millis();
   }
-}*/ //ikke ferdig skal brukes til å finne fargen som leses når ingen biler er i nærrheten
+  if (count == 10) {  // etter 10 målinger vil gjennomsnittet bli lagret
+    for (short i; i <= 2; i++) {
+      base[i] = (base[i] - prevBase[i]) / 10;
+      prevBase[i] = base[i];
+    }
+    count = 0;
+  }
+  return base;
+}
 
-void IDcheck(int baseColor[]) {  //funksjonen som skal identisere fargene
-//denne funksjonen bygger på gammelt design, men tanken er å ta inn data fra kalibreringa ta den dataen minus nåværende 
-//vurColor for å se etter store utslag
+void IDcheck() {  //funksjonen som skal identisere fargene
+                  //denne funksjonen bygger på gammelt design, men tanken er å ta inn data fra kalibreringa ta den dataen minus nåværende
+                  //curColor for å se etter store utslag
+  static uint32_t testTime = millis();
+  int* baseColor;
+  baseColor = calibrateCol();
   int* curColor;
   curColor = colorRead();
   static int colorCheck[3];
-  for (int i; i <= 2; i++) {
-    colorCheck[i] = baseColor[i] - curColor[i];
+  for (short i; i <= 2; i++) {
+    colorCheck[i] = curColor[i] - baseColor[i];
   }
-  Serial.print(colorCheck[0]);
-  Serial.print(colorCheck[1]);
-  Serial.println(colorCheck[2]);
+  if (millis() - testTime > 1000) {
+    Serial.print(colorCheck[0]);
+    Serial.print(colorCheck[1]);
+    Serial.println(colorCheck[2]);
+    testTime = millis();
+  }
 }
 
 void display() {
-  int* colorDis;           //om mann skal kalle på farge detektoren, gjør det slikt
+  /*int* colorDis;           //om mann skal kalle på farge detektoren, gjør det slikt
   colorDis = colorRead();  //------''------
   Serial.print("red: ");
   Serial.print(colorDis[0]);
@@ -86,7 +111,7 @@ void display() {
   Serial.print(" ");
   Serial.print("blue: ");
   Serial.print(colorDis[2]);
-  Serial.print(" ");
+  Serial.print(" ");*/
   /*Serial.print("prox: ");
   Serial.println(proxRead());
   Serial.print("count:");
@@ -95,12 +120,9 @@ void display() {
 }
 
 void loop() {
-  //colorRead();
-  //proxRead();
-  static int potVal;
-  potVal = analogRead(potPin);  //alt med "pot" i seg er bare for testing, skal fjernes fra ferdig produkt
-  if (potVal < 900) display();  //kan starte og stoppe vising av målinger med potmetere (gadd ikke å lage bryter)
-  if (potVal > 2000) IDcheck();
-  //Serial.println(potVal);
-  delay(100);
+  //static int potVal;
+  //potVal = analogRead(potPin);  //alt med "pot" i seg er bare for testing, skal fjernes fra ferdig produkt
+  IDcheck();
+  //if (potVal < 900) display();  //kan starte og stoppe vising av målinger med potmetere (gadd ikke å lage bryter)
+  //if (potVal > 2000) IDcheck();
 }
