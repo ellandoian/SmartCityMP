8bb21267ff2b7a14f56dba27e7c76e27123e85ba
LilLunner <magnus.frafjord@lyse.net>
2024-05-19 15:08:13 +0200

    vscode

---
+++ .arduino/output/libraries/Zumo32U4/Zumo32U4IMU.cpp.d
@@ -0,0 +1,8 @@
+C:\Users\Magnus\Documents\GitHub\SmartCityMP\.arduino\output\libraries\Zumo32U4\Zumo32U4IMU.cpp.o: \
+ C:\Users\Magnus\Documents\Arduino\libraries\Zumo32U4\src\Zumo32U4IMU.cpp \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src/Wire.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Stream.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Print.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WString.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Printable.h \
+ C:\Users\Magnus\Documents\Arduino\libraries\Zumo32U4\src/Zumo32U4IMU.h
--- .arduino/output/sketch/BilForbruk.ino.cpp
+++ .arduino/output/sketch/BilForbruk.ino.cpp
@@ -1,12 +1,13 @@
-# 1 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino"
+#include <Arduino.h>
+#line 1 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino"
 //Antatt forbruk 0.2kWh per km, 400 km rekkevidde. Maks batteri er 80 kWh.
 
-# 4 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino" 2
-# 5 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino" 2
-# 6 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino" 2
-# 7 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino" 2
-# 8 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino" 2
-
+#include <IRremote.h>
+#include <Wire.h>
+#include <Zumo32U4.h>
+#include <EEPROM.h>
+#include <WiFi.h>
+ 
 Zumo32U4Motors motors;
 Zumo32U4ButtonA buttonA;
 Zumo32U4ButtonB buttonB;
@@ -24,16 +25,33 @@ float partDisGlobal;
 //Avstand kjørt, 1m kjøring er 10km simulert kjøring.
 //Etter 255km kjørt simulert, deles totaldistansen opp i et multiplum av 255 og en rest, slik at EEprom kan lagre hele distansen.
 
+#line 26 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino"
+float distMeasure();
+#line 44 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino"
+int batteryDrain(int battery);
+#line 55 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino"
+void showBattery();
+#line 69 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino"
+void Receive(int howMany);
+#line 82 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino"
+void Charge();
+#line 89 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino"
+void sendDistance();
+#line 95 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino"
+void setup();
+#line 110 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino"
+void loop();
+#line 26 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino"
 float distMeasure()
 {
     if (partDisGlobal>=255) {
         distMultiplier += 1;
         partDisGlobal -= 255;
     }
-    int currRotLeft = encoder.getCountsAndResetLeft();
+    int currRotLeft = encoder.getCountsAndResetLeft();              
     int currRotRight = encoder.getCountsAndResetRight();
-    float leftDist = ((((currRotLeft)>0?(currRotLeft):-(currRotLeft)))*3.1415*0.039)/910;
-    float rightDist = ((((currRotRight)>0?(currRotRight):-(currRotRight)))*3.1415*0.039)/910;
+    float leftDist = ((abs(currRotLeft))*3.1415*0.039)/910;
+    float rightDist = ((abs(currRotRight))*3.1415*0.039)/910;
     float distPart = partDisGlobal + (10*(leftDist + rightDist)/2);
     EEPROM.write(1, partDisGlobal);
     EEPROM.write(2, distMultiplier);
@@ -49,27 +67,19 @@ int batteryDrain(int battery) {
     }
     EEPROM.write(0, battery);
     return battery;
-}
+}   
 
 //Viser batteriet og avstand kjørt på displayet
 
 void showBattery() {
-    display.gotoXY(0, 0);
-    display.print((reinterpret_cast<const __FlashStringHelper *>(
-# 57 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino" 3
-                 (__extension__({static const char __c[] __attribute__((__progmem__)) = (
-# 57 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino"
-                 "Power: "
-# 57 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino" 3
-                 ); &__c[0];}))
-# 57 "C:\\Users\\Magnus\\Documents\\GitHub\\SmartCityMP\\BilForbruk\\BilForbruk.ino"
-                 )));
+    display.gotoXY(0, 0); 
+    display.print(F("Power: ")); 
     display.gotoXY(0, 1);
-    display.print(power);
-    display.gotoXY(0, 3);
+    display.print(power); 
+    display.gotoXY(0, 3); 
     display.println("Distance drove; ");
     display.gotoXY(0, 4);
-    display.print(totalDistance);
+    display.print(totalDistance); 
     display.print("km");
 }
 
@@ -79,7 +89,7 @@ void Receive(int howMany) {
     while(0 < Wire.available()) // loop through all but the last
   {
     int c = Wire.read(); // receive byte as a character
-    Serial.println(c); // print the character
+    Serial.println(c);         // print the character
     if (c == 5) {
         Charge();
     }
