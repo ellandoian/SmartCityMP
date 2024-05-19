757e19cd365fa045e455c66f713cff63e4bec9c8
LilLunner <magnus.frafjord@lyse.net>
2024-05-19 13:44:52 +0200

    gitIgnore

---
+++ gitIgnore/.arduino/output/libraries/PololuOLED/font.cpp.d
@@ -0,0 +1,19 @@
+C:\Users\Magnus\Documents\GitHub\SmartCityMP\.arduino\output\libraries\PololuOLED\font.cpp.o: \
+ C:\Users\Magnus\Documents\Arduino\libraries\PololuOLED\src\font.cpp \
+ C:\Users\Magnus\Documents\Arduino\libraries\PololuOLED\src/PololuOLED.h \
+ C:\Users\Magnus\Documents\Arduino\libraries\PololuOLED\src/PololuSH1106.h \
+ C:\Users\Magnus\Documents\Arduino\libraries\PololuOLED\src/PololuSH1106Main.h \
+ C:\Users\Magnus\Documents\Arduino\libraries\PololuOLED\src/PololuOLEDHelpers.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/binary.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WCharacter.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WString.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/HardwareSerial.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Stream.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Print.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Printable.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/USBAPI.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/USBDesc.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/USBCore.h \
+ C:\Users\Magnus\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\variants\leonardo/pins_arduino.h
