3d4bcfb814d9d852b0d182755125a353695b4d0f
ellandoian <jjojjo3214@gmail.com>
2024-05-19 14:58:02 +0200

    added mqtt

--- bommring/bommring.ino
+++ bommring/bommring.ino
@@ -1,12 +1,92 @@
 #include <Arduino_APDS9960.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <Wire.h>
+
+// wifi og wifipassord
+const char* ssid = "NTNU-IOT";
+const char* password = "";
+
+//Broker adresse
+const char* mqtt_server = "10.25.18.138";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+
 int pushButton = 25;
 
 void setup() {
   Serial.begin(115200);
   APDS.begin();
   pinMode(pushButton, INPUT);
+  Serial.println("start");
+  // mqtt settup
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void setup_wifi() {
+  delay(10);
+  // Kobler til wifi:
+  Serial.println();
+  Serial.print("Kobler til: ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi kobling opprettet");
+  Serial.println("IP addresse: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Melding ankommet topic: ");
+  Serial.print(topic);
+  Serial.print(". Melding: ");
+  String messageTemp;
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  if (String(topic) == "esp32/output") {
+    Serial.print("Endrer output til: ");
+    if (messageTemp == "on") {
+      Serial.println("på");
+    } else if (messageTemp == "off") {
+      Serial.println("av");
+    }
+  }
+}
+
+void reconnect() {
+  client.subscribe("esp32/output");
+  // Looper til en kobling er opprettet
+  while (!client.connected()) {
+    Serial.print("Forsøker å opprette kobling til mqtt...");
+    // Attempt to connect
+    if (client.connect("Quagmire_publisher", "njaal", "3Inshallah4")) {
+      Serial.println("connected");
+      // Topic som det subscribes til
+      client.subscribe("esp32/output");
+    } else {
+      Serial.print("mislykket kobling, rc=");
+      Serial.print(client.state());
+      Serial.println(" Prøver igjen om 5 sekund");
+      delay(5000);
+    }
+  }
 }
 
 //kilde, ellandoian/filedump
@@ -120,17 +200,17 @@ String IDcheck() {  //funksjonen som skal identisere fargene
   return ID;
 }
 
-void printOnce() { //printer kun når det er ny informasjon, og om den lagra informasjonen har hendt de siste 50 nye avlesningene vil det heller ikke bli printet
+void printOnce() {  //printer kun når det er ny informasjon, og om den lagra informasjonen har hendt de siste 50 nye avlesningene vil det heller ikke bli printet
   static String prevInput = IDcheck();
   static String dataArr[50] = { String(0) };
-  static uint32_t dataTime[50] = {millis()};
+  static uint32_t dataTime[50] = { millis() };
   static int j;
   static bool io = false;
   j++;
   if (j < 50) {
     j = 0;
   }
-  if (prevInput != IDcheck()) { 
+  if (prevInput != IDcheck()) {
     String data = IDcheck();
     //Serial.println(IDcheck());
     for (int i; i <= 50; i++) {
@@ -140,7 +220,11 @@ void printOnce() { //printer kun når det er ny informasjon, og om den lagra inf
       } else io = false;
     }
     if (io == false) {
-      Serial.println(data); //putt "push" greia her
+      int length = data.length();      // kilde https://www.geeksforgeeks.org/convert-string-char-array-cpp/
+      char* sendArr = new char[length + 1];  // -----""-----
+      strcpy(sendArr, data.c_str());         // -----""-----
+      Serial.println(data);                  //putt "push" greia her
+      client.publish("esp32/output", sendArr);
       dataArr[j] = data;
     }
     prevInput = IDcheck();
@@ -148,5 +232,9 @@ void printOnce() { //printer kun når det er ny informasjon, og om den lagra inf
 }
 
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
   printOnce();
 }
