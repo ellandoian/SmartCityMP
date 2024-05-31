86018c3b8594f84abfc01f5bb7fc1e6a8a8c03e9
ellandoian <jjojjo3214@gmail.com>
2024-05-31 14:09:45 +0200

    Fletta bilForbruk og carCode sammen

--- BilForbruk/BilForbruk.ino
+++ BilForbruk/BilForbruk.ino
@@ -3,7 +3,13 @@
 #include <Wire.h>
 #include <Zumo32U4.h>
 #include <EEPROM.h>
-
+/*
+******************************************************************************************************************
+***************************************************|----------|***************************************************
+***************************************************|LES README|***************************************************
+***************************************************|----------|***************************************************
+******************************************************************************************************************
+*/
 Zumo32U4Motors motors;
 Zumo32U4ButtonC buttonC;
 Zumo32U4LineSensors lineSensors;
@@ -11,11 +17,13 @@ Zumo32U4OLED display;
 Zumo32U4Encoders encoder;
 
 byte topSpeed = 200;
+bool pidFlag = true;  //for å kunne tvinge PID av
 byte power, distMultiplier, input;
 unsigned long totalDistance;
 float partDisGlobal;
-int courseArray[]={};
+int courseArray[] = {};
 bool sendChargeDist = false;
+static int drip[5];  //trengs for å kunne lese av spesfik sensor
 
 //Avstand kjørt, 1m kjøring er 10km simulert kjøring.
 //Etter 255km kjørt simulert, deles totaldistansen opp i et multiplum av 255 og en rest, slik at EEprom kan lagre hele distansen.
@@ -67,7 +75,7 @@ void Receive(int howMany) {
   while (0 < Wire.available())  // loop through all
   {
     byte receivedByte = Wire.read();
-    courseArray[i] = receivedByte - '0'; // Convert from ASCII to integer
+    courseArray[i] = receivedByte - '0';  // Convert from ASCII to integer
     Serial.println(courseArray[i]);
     i++;
   }
@@ -78,9 +86,9 @@ void Receive(int howMany) {
 void Charge() {
   display.clear();
   display.println("CHARGING");
-  sendChargeDist = true; //Flagg som gjør at bilen sender distansen til ESP32
+  sendChargeDist = true;  //Flagg som gjør at bilen sender distansen til ESP32
   motors.setSpeeds(0, 0);
-  delay(1000); //Skal bort senere
+  delay(1000);  //Skal bort senere
 }
 
 //Sende distanse kjørt til ESP, kjøres når bilen lader
@@ -93,7 +101,7 @@ void sendDistance() {
     Wire.write(distMultiplier);
     sendChargeDist = false;
     distMultiplier = 0;
-    partDisGlobal = 0; //Resetter avstanden etter den er sendt
+    partDisGlobal = 0;  //Resetter avstanden etter den er sendt
   }
 }
 
@@ -105,14 +113,100 @@ short lineSensorRead() {
 
 void lineFollowPID(int pos) {  // tar inn posisjonen
   static short prevPos;
-  short correction = pos / 4 + 6 * (pos - prevPos);  // kilde eksempelkode
-  prevPos = pos;
-  byte lSpeed = constrain(topSpeed + correction, 0, topSpeed);  // farten på venstre side lik topSpeed + correction
-  byte rSpeed = constrain(topSpeed - correction, 0, topSpeed);  // farten på høgre side lik topspeed - correction
-                                                                // setter slik at verdien vil alltids være mellom 200 og 0, vil forhindre for høye hastigheter, men viktigs
-                                                                // hindrer at det vil fort gå fra positiv hastighet til negativ hastighet som kan skade motorene.
-  Serial.println(lSpeed);
-  motors.setSpeeds(lSpeed, rSpeed);
+  if (pidFlag) {
+    short correction = pos / 4 + 6 * (pos - prevPos);  // kilde eksempelkode
+    prevPos = pos;
+    byte lSpeed = constrain(topSpeed + correction, 0, topSpeed);  // farten på venstre side lik topSpeed + correction
+    byte rSpeed = constrain(topSpeed - correction, 0, topSpeed);  // farten på høgre side lik topspeed - correction
+                                                                  // setter slik at verdien vil alltids være mellom 200 og 0, vil forhindre for høye hastigheter, men viktigs
+                                                                  // hindrer at det vil fort gå fra positiv hastighet til negativ hastighet som kan skade motorene.
+    motors.setSpeeds(lSpeed, rSpeed);
+  }
+}
+
+void drivingMain() {
+  int filler[3] = { 3, 2, 1 };
+  switch (input) {
+    case 1:
+      static bool leftFlag = false;
+      static bool leftFlag2 = true;
+      static byte leftCounter = 0;
+      static uint32_t leftTime = millis();
+      lineFollowPID(lineSensorRead());
+      if (lineSensors.readOneSens(drip) >= 900) {  //merker at den rører en linje og setter av et flag
+        leftFlag = true;
+      } else if (lineSensors.readOneSens(drip) < 100 && leftFlag) {  //når bilen har gått av linjen flippes flaget tilbake og counter går +1
+        leftCounter++;
+        leftFlag = false;
+      }
+
+      if (lineSensors.readOneSens(drip) >= 800 && leftCounter == 1) {  //når bilen kommer til en linje etter å ha pasert en vil den svinge til venstre
+        motors.setSpeeds(-100, 100);
+        leftTime = millis();
+        leftFlag2 = false;
+        pidFlag = false;  //skrur av PID kjøring
+      }
+      if (leftFlag2 == false && millis() - leftTime >= 500) {  //avsluttersvingen og skrur på PID kjøring
+        leftFlag2 = true;
+        Serial.println("turn Complete");
+        pidFlag = true;
+      }
+      if (leftCounter >= 3) {  //tar å resetter counter og fullfører denne svingen etter bilen er ute av kryset
+        leftCounter = 0;
+        input = 4;
+        break;
+      }
+      break;
+    case 2:
+      static bool straightFlag = false;
+      static byte straightCounter = 0;
+      if (straightCounter < 2) {  //fjern if setningen
+        lineFollowPID(lineSensorRead());
+      }
+      if (lineSensors.readOneSens(drip) >= 900) straightFlag = true;  //merker at den har kommet på en svart linje på venstre side av bilen
+      else if (lineSensors.readOneSens(drip) == 0 && straightFlag) {  //teller + 1 etter bilen har pasert linja
+        straightCounter++;                                            //
+        straightFlag = false;                                         //
+      }
+      if (straightCounter >= 2) {  //om den har pasert to linjer går den videre til neste steg
+        straightCounter = 0;
+        input = 4;
+        break;
+      }
+      break;
+    case 3:
+      static bool rightFlag = false;
+      static uint32_t rightTime = millis();
+      if (lineSensors.readOneSens(drip) >= 900) {  //Om bilen har kommet til et kryss vil den svinge til høyere
+        rightTime = millis();
+        motors.setSpeeds(150, -100);
+        Serial.println("truning Right");
+        rightFlag = true;
+      }
+      if (millis() - rightTime >= 500 && rightFlag) {  //om bilen har fullført svingen hopper bilen til neste case
+        input = 4;
+        rightFlag = false;
+        break;
+      } else if (millis() - rightTime >= 500) lineFollowPID(lineSensorRead());  //kjører PID om ingen sving
+      break;
+    case 4:
+      delay(1000);
+      motors.setSpeeds(0, 0);
+      /*static bool switcher = true;
+      static uint32_t switcherTime = millis();
+      lineFollowPID(lineSensorRead());
+      if (swithcer) {
+        switcherTime = millis();
+        switcher = false;
+      }
+      if(millis()-switcherTime >= 2000){
+        
+      }*/
+    case 5:
+      Charge();
+      break;
+    default:
+  }
 }
 
 void pidSetup() {
@@ -126,24 +220,6 @@ void pidSetup() {
     if (millis() - startTime >= 4000) startFlag = false;
   }
   motors.setSpeeds(0, 0);
-}
-
-void drivingMain() {
-  int filler[3] = { 3, 2, 1 };
-  switch (input) {
-    case 1:
-      break;
-    case 2:
-      break;
-    case 3:
-      break;
-    case 4:
-        Charge();
-
-        break;
-    default:
-      lineFollowPID(lineSensorRead());
-  }
 }
 
 //Main
@@ -164,13 +240,13 @@ void setup() {
 }
 
 void loop() {
-  motors.setSpeeds(100,100); //skal bort
-  static long tid; //skal bort
+  motors.setSpeeds(100, 100);  //skal bort
+  static long tid;             //skal bort
   partDisGlobal = distMeasure();
   totalDistance = partDisGlobal + (distMultiplier * 255);
   power = batteryDrain(power);
   showBattery();
-  if (millis()-tid >= 5000) { //if-setningen skal bort
+  if (millis() - tid >= 5000) {  //if-setningen skal bort
     Charge();
     tid = millis();
   }
